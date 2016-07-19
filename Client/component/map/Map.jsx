4de3feeea0b05b3f9120f97b14a549806850e2da
Ryan McChesney <rmcchesn@gmail.com>
2016-07-18 20:02:37 -0700

    Add default styling to index.html - to be moved to main css file later. Update default coordinates on mapbox.

--- Client/component/map/Map.jsx
+++ Client/component/map/Map.jsx
@@ -1,8 +1,10 @@
+var defaultCoord = [37.7837008, -122.4111551];
+
 L.mapbox.accessToken = 'pk.eyJ1Ijoicm1jY2hlc24iLCJhIjoiY2lxbHkxbXFiMDA5dWZubm5mNWkwdGYwbiJ9.QC1lP-2tNymbJ5tHaMugZw';
 
 var Map = React.createClass({
   componentDidMount() {
-    this.map = L.mapbox.map('map-one', 'mapbox.streets').setView([38.8929, -77.0252], 14);
+    this.map = L.mapbox.map('map-one', 'mapbox.streets').setView(defaultCoord, 14);
   },
 
   render() {
